bda492f3a5113eda3576dbcd6e779607f9d38857
choijihyeon <gus1043@gmail.com>
2023-08-04 19:07:21 +0900

    feat : 하단메뉴 및 fragment 생성

---
+++ app/src/main/java/com/example/myapplication/Fragment1.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.Fragment1Binding
+
+class Fragment1 : Fragment() {
+    private var _binding: Fragment1Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = Fragment1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.login.setOnClickListener {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.signup.setOnClickListener {
+            val intent = Intent(requireContext(), SignupActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,41 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myapplication.*
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var Fragment1: Fragment1
+    private lateinit var Fragment2: Fragment2
+    private lateinit var Fragment3: Fragment3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-        binding.login.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+
+        Fragment1 = Fragment1()
+        Fragment2 = Fragment2()
+        Fragment3 = Fragment3()
+
+        supportFragmentManager.beginTransaction().replace(R.id.container, Fragment1).commit()
+
+        binding.bottomMenu.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.tab_home -> replaceFragment(Fragment1)
+                R.id.tab_bookmark -> replaceFragment(Fragment2)
+                R.id.tab_mypage -> replaceFragment(Fragment3)
+            }
+            true
         }
 
-        binding.signup.setOnClickListener {
-            val intent = Intent(this, SignupActivity::class.java)
-            startActivity(intent)
-        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
     }
 }
