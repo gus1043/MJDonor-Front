74cd80737670428f7066016041196395335b4bc1
choijihyeon <gus1043@gmail.com>
2023-08-05 07:11:15 +0900

    fix : main 화면 activity에 출력->fragment1에 출력으로 수정

--- app/src/main/java/com/example/myapplication/Fragment1.kt
+++ app/src/main/java/com/example/myapplication/Fragment1.kt
@@ -1,41 +1,16 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myapplication.databinding.Fragment1Binding
 
 class Fragment1 : Fragment() {
-    private var _binding: Fragment1Binding? = null
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = Fragment1Binding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.login.setOnClickListener {
-            val intent = Intent(requireContext(), LoginActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.signup.setOnClickListener {
-            val intent = Intent(requireContext(), SignupActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return inflater.inflate(R.layout.fragment_1, container, false)
     }
 }
--- app/src/main/java/com/example/myapplication/Fragment3.kt
+++ app/src/main/java/com/example/myapplication/Fragment3.kt
@@ -1,16 +1,41 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.Fragment3Binding
 
 class Fragment3 : Fragment() {
+    private var _binding: Fragment3Binding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_3, container, false)
+        _binding = Fragment3Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.login.setOnClickListener {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.signup.setOnClickListener {
+            val intent = Intent(requireContext(), SignupActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
