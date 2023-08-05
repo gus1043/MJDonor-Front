04c564732e160be9b6922f11bc5602e770bc14a5
choijihyeon <gus1043@gmail.com>
2023-08-05 09:51:59 +0900

    feat : 기부금 등록 페이지

---
+++ app/src/main/java/com/example/myapplication/DonInputActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityDonateinputBinding
+import com.example.myapplication.databinding.ActivityDonationselectionBinding
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class DonInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDonateinputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.inputdonatebtn.setOnClickListener {
+//            val intent: Intent = Intent(this, DonInputActivity::class.java)
+//            startActivity(intent)
+        }
+    }
+}
