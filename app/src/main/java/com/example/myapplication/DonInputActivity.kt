77b9dd0fbf1e8e95a3de281192069b9a521362c8
KimSH39 <sooooooohyeon5@naver.com>
2023-08-10 19:20:41 +0900

    feat: 기부금액 input

--- app/src/main/java/com/example/myapplication/DonInputActivity.kt
+++ app/src/main/java/com/example/myapplication/DonInputActivity.kt
@@ -1,21 +1,75 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityDonateinputBinding
 import com.example.myapplication.databinding.ActivityDonationselectionBinding
 import com.example.myapplication.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class DonInputActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDonateinputBinding
+    private lateinit var currencyFormat: NumberFormat
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityDonateinputBinding.inflate(layoutInflater)
+        binding = ActivityDonateinputBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        currencyFormat = NumberFormat.getCurrencyInstance()
+
+        // EditText 초기값 설정
+        binding.money.setText("0")
+
+        binding.money.textAlignment = EditText.TEXT_ALIGNMENT_TEXT_END
+
+        binding.money.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                // 입력된 내용을 가져와서 숫자 형식으로 변환
+                val input = s.toString().replace("[^\\d]".toRegex(), "")
+                val parsed = if (input.isEmpty()) 0 else input.toInt()
+                val formatted = currencyFormat.format(parsed)
+                binding.money.removeTextChangedListener(this)
+                binding.money.setText(formatted)
+                binding.money.setSelection(formatted.length)
+                binding.money.addTextChangedListener(this)
+            }
+        })
+
         binding.inputdonatebtn.setOnClickListener {
-//            val intent: Intent = Intent(this, DonInputActivity::class.java)
-//            startActivity(intent)
+            // TODO: 입력된 금액 처리
+            // 예: 입력된 금액을 다음 단계로 전달하거나 로직을 수행
+        }
+
+        binding.button1.setOnClickListener {
+            // 현재 입력된 금액에 1000을 더해서 업데이트
+            val currentAmount = binding.money.text.toString().replace("[^\\d]".toRegex(), "").toInt()
+            val newAmount = currentAmount + 1000
+            binding.money.setText(newAmount.toString())
+        }
+
+        binding.button2.setOnClickListener {
+            // 현재 입력된 금액에 5000을 더해서 업데이트
+            val currentAmount = binding.money.text.toString().replace("[^\\d]".toRegex(), "").toInt()
+            val newAmount = currentAmount + 5000
+            binding.money.setText(newAmount.toString())
+        }
+
+        binding.button3.setOnClickListener {
+            // 현재 입력된 금액에 10000을 더해서 업데이트
+            val currentAmount = binding.money.text.toString().replace("[^\\d]".toRegex(), "").toInt()
+            val newAmount = currentAmount + 10000
+            binding.money.setText(newAmount.toString())
         }
     }
 }
