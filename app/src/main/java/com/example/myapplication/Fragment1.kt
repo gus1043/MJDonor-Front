44c28a66964db416a5f0432f6c80c38def5a8f67
choijihyeon <gus1043@gmail.com>
2023-08-14 21:15:32 +0900

    feat : 회원가입 stepper구현, 로그인 버튼 백그라운드

--- app/src/main/java/com/example/myapplication/Fragment1.kt
+++ app/src/main/java/com/example/myapplication/Fragment1.kt
@@ -4,13 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.LoginActivity
 import com.example.myapplication.R
 import com.example.myapplication.SignupActivity
+import com.example.myapplication.SignupStepActivity
 import com.example.myapplication.databinding.Fragment1Binding
-import com.example.myapplication.databinding.Fragment3Binding
 
 class Fragment1 : Fragment() {
     private lateinit var recyclerViewmain: RecyclerView
@@ -43,6 +41,11 @@ class Fragment1 : Fragment() {
 
         // Set up the carousel layout manager for the RecyclerView
         recyclerViewmain.setCarouselLayoutManager()
+
+        binding.signup.setOnClickListener {
+            val intent = Intent(requireContext(), SignupStepActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
