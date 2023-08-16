b53e8f6f32d9e33fabf093cb32cb11e8d59d065a
choijihyeon <gus1043@gmail.com>
2023-08-17 08:25:13 +0900

    feat : 프로필 이미지 입력 step 완료

--- app/src/main/java/com/example/myapplication/SignupStepActivity.kt
+++ app/src/main/java/com/example/myapplication/SignupStepActivity.kt
@@ -3,12 +3,21 @@ package com.example.myapplication
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.myapplication.databinding.ActivityRegisterBinding
 import com.example.myapplication.databinding.ActivitySignupstepBinding
 import java.util.regex.Pattern
 
@@ -16,18 +25,22 @@ class SignupStepActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupstepBinding
     private var position = 0
-
     val TAG: String = "회원가입"
-    var isExistBlank = false
-    var isPWSame = false
-    var studentNumCorrect = true
-    var passwordCorrect = true
-    var emailCorrect = true
+
+    private lateinit var imageView1: ImageView
+    private lateinit var imageView2: ImageView
+    private lateinit var imageView3: ImageView
+
+    private val GET_IMAGE_FOR_IMAGEVIEW1 = 201
+    private val GET_IMAGE_FOR_IMAGEVIEW2 = 202
+    private val GET_IMAGE_FOR_IMAGEVIEW3 = 203
+
 
     companion object {
         private const val STEP_1 = 0
         private const val STEP_2 = 1
-        private const val FINAL_STEP = 2
+        private const val STEP_3 = 2
+        private const val FINAL_STEP = 3
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +48,16 @@ class SignupStepActivity : AppCompatActivity() {
         binding = ActivitySignupstepBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+
+        imageView1 = findViewById(R.id.imageView1)
+
+        imageView1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+            startActivityForResult(intent, GET_IMAGE_FOR_IMAGEVIEW1)
+        }
+
+
         binding.stepView.done(false)
         binding.tologinpbtn.setOnClickListener{
             finish()
@@ -46,8 +69,17 @@ class SignupStepActivity : AppCompatActivity() {
         binding.button.setOnClickListener {
             when (position) {
                 STEP_1 -> transitionToStep(STEP_2, "Next")
-                STEP_2 -> transitionToStep(FINAL_STEP, "Submit")
+                STEP_2 -> transitionToStep(STEP_3, "Next")
+                STEP_3 -> transitionToStep(FINAL_STEP, "Submit")
                 else -> {
+
+                    var isExistBlank = false
+                    var isPWSame = false
+                    var isAgree = binding.signupAgree.isChecked
+                    var studentNumCorrect = true
+                    var passwordCorrect = true
+                    var emailCorrect = true
+
                     val email = binding.email.text.toString()
                     val password = binding.password.text.toString()
                     val passwordCheck = binding.passwordCheck.text.toString()
@@ -55,14 +87,10 @@ class SignupStepActivity : AppCompatActivity() {
                     val studentNum = binding.studentNum.text.toString()
                     val walletAdress = binding.walletAddress.text.toString()
 
+
                     // 유저가 항목을 다 채우지 않았을 경우
                     if(email.isEmpty() || password.isEmpty() || passwordCheck.isEmpty()|| name.isEmpty()|| studentNum.isEmpty()|| walletAdress.isEmpty()){
                         isExistBlank = true
-                    }
-                    else{
-                        if(password == passwordCheck){
-                            isPWSame = true
-                        }
                     }
 
                     //이메일 유효성 체크
@@ -76,13 +104,22 @@ class SignupStepActivity : AppCompatActivity() {
                         studentNumCorrect= false
                     }
 
+                    if(!binding.signupAgree.isChecked){
+                        isAgree = false
+                    } else {
+                        isAgree = true
+                    }
+
                     //비밀번호 유효성
                     if(!Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,20}$", password))
                     {
-                       passwordCorrect= false
-                    }
+                       passwordCorrect= false }
 
-                    if (!isExistBlank && isPWSame && emailCorrect && studentNumCorrect && passwordCorrect) {
+                    if(password == passwordCheck){
+                        isPWSame = true }
+
+
+                    if (!isExistBlank && isPWSame && emailCorrect && studentNumCorrect && passwordCorrect && isAgree) {
                         // 회원가입 성공 토스트 메세지 띄우기
                         Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
 
@@ -114,6 +151,9 @@ class SignupStepActivity : AppCompatActivity() {
                         else if(!studentNumCorrect){ // 입력한 비밀번호가 다를 경우
                             dialog("studentNumCorrect")
                         }
+                        else if(!isAgree){ // 이용약관 동의 안한 경우
+                            dialog("Agree")
+                        }
                     }
                 }
             }
@@ -124,7 +164,8 @@ class SignupStepActivity : AppCompatActivity() {
         when (position) {
             STEP_1 -> super.onBackPressed()
             STEP_2 -> transitionToStep(STEP_1, "Next")
-            else -> transitionToStep(STEP_2, "Next")
+            STEP_3 -> transitionToStep(STEP_2, "Next")
+            else -> transitionToStep(STEP_3, "Next")
         }
     }
 
@@ -132,6 +173,7 @@ class SignupStepActivity : AppCompatActivity() {
         when (position) {
             STEP_1 -> binding.STEP1.visibility = View.GONE
             STEP_2 -> binding.STEP2.visibility = View.GONE
+            STEP_3 -> binding.STEP3.visibility = View.GONE
             FINAL_STEP -> binding.FINALSTEP.visibility = View.GONE
         }
         position = nextPosition
@@ -141,6 +183,7 @@ class SignupStepActivity : AppCompatActivity() {
         when (position) {
             STEP_1 -> binding.STEP1.visibility = View.VISIBLE
             STEP_2 -> binding.STEP2.visibility = View.VISIBLE
+            STEP_3 -> binding.STEP3.visibility = View.VISIBLE
             FINAL_STEP -> binding.FINALSTEP.visibility = View.VISIBLE
         }
     }
@@ -176,6 +219,10 @@ class SignupStepActivity : AppCompatActivity() {
             dialog.setTitle("회원가입 실패")
             dialog.setMessage("비밀번호 형식을 맞춰 주세요")
         }
+        else if(type.equals("Agree")){
+            dialog.setTitle("회원가입 실패")
+            dialog.setMessage("이용약관에 동의해주세요")
+        }
 
         val dialog_listener = object: DialogInterface.OnClickListener{
             override fun onClick(dialog: DialogInterface?, which: Int) {
@@ -189,4 +236,18 @@ class SignupStepActivity : AppCompatActivity() {
         dialog.setPositiveButton("확인",dialog_listener)
         dialog.show()
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val selectedImageUri: Uri?
+        val option1 = RequestOptions.bitmapTransform(MultiTransformation(FitCenter(), RoundedCorners(30)))
+
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && data != null && data.data != null) {
+            selectedImageUri = data.data
+            when (requestCode) {
+                GET_IMAGE_FOR_IMAGEVIEW1 -> Glide.with(applicationContext).load(selectedImageUri).apply(option1).into(imageView1)
+            }
+        }
+    }
+
+
 }
