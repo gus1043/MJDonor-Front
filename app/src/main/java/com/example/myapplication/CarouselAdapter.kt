da49971b2d1d5875d006426724aca1ec806b1a58
choijihyeon <gus1043@gmail.com>
2023-08-24 05:19:31 +0900

    fix : 모금액, 마감일 연결 수정

--- app/src/main/java/com/example/myapplication/CarouselAdapter.kt
+++ app/src/main/java/com/example/myapplication/CarouselAdapter.kt
@@ -83,7 +83,9 @@ class CarouselAdapter(private val context: Context, private val itemList: List<C
                 intent.putExtra("enddate", currentItem.enddate)
                 intent.putExtra("current", currentItem.current)
                 context.startActivity(intent)
-
+                Log.d("carousel", currentItem.current)
+                Log.d("carousel", currentItem.title)
+                Log.d("carousel", currentItem.goal)
                 if (context is Activity) {
                     context.overridePendingTransition(R.anim.fromright_toleft, R.anim.none)
                 }
