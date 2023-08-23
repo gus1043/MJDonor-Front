767b91240c938506bb520022d571c121b04ca814
choijihyeon <gus1043@gmail.com>
2023-08-23 14:47:54 +0900

    feat : 미로그인시 캐러셀 클릭 시 로그인 이동

--- app/src/main/java/com/example/myapplication/CarouselAdapter.kt
+++ app/src/main/java/com/example/myapplication/CarouselAdapter.kt
@@ -1,16 +1,19 @@
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.myapplication.DonSelectActivity
+import com.example.myapplication.LoginActivity
 import com.example.myapplication.R
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
@@ -21,7 +24,7 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class CarouselAdapter(private val context: Context, private val itemList: List<CarouselItem>) : RecyclerView.Adapter<CarouselAdapter.ViewHolder>() {
-
+    private lateinit var sharedPreferences: SharedPreferences
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageButton: ImageButton = itemView.findViewById(R.id.imageButton)
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
@@ -30,6 +33,7 @@ class CarouselAdapter(private val context: Context, private val itemList: List<C
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_donation_main, parent, false)
+        sharedPreferences = context.getSharedPreferences("Account", Context.MODE_PRIVATE)
         return ViewHolder(itemView)
     }
 
@@ -56,21 +60,33 @@ class CarouselAdapter(private val context: Context, private val itemList: List<C
 
         holder.titleTextView.text = currentItem.title
         holder.descriptionTextView.text = currentItem.donLoc
-        holder.imageButton.setOnClickListener {
-            val intent = Intent(context, DonSelectActivity::class.java)
-            intent.putExtra("title", currentItem.title)
-            intent.putExtra("donLoc", currentItem.donLoc)
-            intent.putExtra("image1", currentItem.image1)
-            intent.putExtra("image2", currentItem.image2)
-            intent.putExtra("p_id", currentItem.p_id)
-            intent.putExtra("description", currentItem.description)
-            intent.putExtra("goal", currentItem.goal)
-            intent.putExtra("enddate", currentItem.enddate)
-            intent.putExtra("current", currentItem.current)
-            context.startActivity(intent)
 
-            if (context is Activity) {
-                context.overridePendingTransition(R.anim.fromright_toleft, R.anim.none)
+
+        val sharedPreferences = context.getSharedPreferences("login", Context.MODE_PRIVATE) // 앱 자체에 데이터 저장
+        val isFirstTime = sharedPreferences.getBoolean("login", false)
+        holder.imageButton.setOnClickListener {
+            if (!isFirstTime) {
+                val intent = Intent(context, LoginActivity::class.java)
+                context.startActivity(intent)
+                if (context is Activity) {
+                    context.overridePendingTransition(R.anim.fromright_toleft, R.anim.none)
+                }
+            }else {
+                val intent = Intent(context, DonSelectActivity::class.java)
+                intent.putExtra("title", currentItem.title)
+                intent.putExtra("donLoc", currentItem.donLoc)
+                intent.putExtra("image1", currentItem.image1)
+                intent.putExtra("image2", currentItem.image2)
+                intent.putExtra("p_id", currentItem.p_id)
+                intent.putExtra("description", currentItem.description)
+                intent.putExtra("goal", currentItem.goal)
+                intent.putExtra("enddate", currentItem.enddate)
+                intent.putExtra("current", currentItem.current)
+                context.startActivity(intent)
+
+                if (context is Activity) {
+                    context.overridePendingTransition(R.anim.fromright_toleft, R.anim.none)
+                }
             }
         }
 
